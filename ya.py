7322f11e1712660da124cbd0a0c9c8fedbc0a45b
kmhmubin <kmh.mubin@nortsouth.edu>
2019-08-10 13:47:25 +0600

    add comment

--- ya.py
+++ ya.py
@@ -2,14 +2,19 @@
 
 import yagmail
 
-receiver = "mygmail@gmail.com"
-body = "Hello there from Yagmail"
-filename = "readme.md"
+receiver = "mygmail@gmail.com" #receiver email address
+body = "Hello there from Yagmail"#email body
+filename = "readme.md"#attach the file
 
+#mail information
 yag = yagmail.SMTP("mygmail@gmail.com", "mypassword")
+
+#sent the mail
 yag.send(
     to=receiver,
-    subject="Yagmail test with attachment",
+    subject="Yagmail test with attachment", # email subject
     contents=body,
     attachments=filename,
 )
+
+
